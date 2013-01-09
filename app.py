ea59b00cfc67157040b7a43894f7250f94850041
deciob <deciob@deciob.(none)>
2013-01-09 21:15:25 +0000

    removed debug mode

--- app.py
+++ app.py
@@ -21,6 +21,6 @@ def home():
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    app.debug = True
+    # app.debug = True
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
